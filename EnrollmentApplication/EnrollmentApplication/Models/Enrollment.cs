d117b3f8e25dbd2126d91665500ac2fc710c850a
Natasha Ostrander <mostrander181@gmail.com>
2019-09-16 10:06:24 -0400

    Initial commit for Lab 4: EnrollmentApplication; setup prior to building EnrollmentController

---
+++ EnrollmentApplication/EnrollmentApplication/Models/Enrollment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EnrollmentApplication.Models
+{
+   public class Enrollment
+   {
+
+      public virtual int EnrollmentId { get; set; }
+      public virtual Student StudentId { get; set; }
+      public virtual Course CourseId { get; set; }
+      public virtual int Grade { get; set; }
+      public virtual Student Student { get; set; }
+      public virtual Course Course { get; set; }
+
+   }
+}
