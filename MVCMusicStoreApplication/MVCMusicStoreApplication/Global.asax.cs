bea9de161658352c284cacd6b9ba7aa62bd2beff
Natasha Ostrander <mostrander181@gmail.com>
2019-09-16 08:03:20 -0400

    Updated tutorial project

--- MVCMusicStoreApplication/MVCMusicStoreApplication/Global.asax.cs
+++ MVCMusicStoreApplication/MVCMusicStoreApplication/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Data.Entity;
+using MVCMusicStoreApplication.Models;
 
 namespace MVCMusicStoreApplication
 {
@@ -12,6 +14,8 @@ namespace MVCMusicStoreApplication
    {
       protected void Application_Start()
       {
+         Database.SetInitializer(new SampleData());
+
          AreaRegistration.RegisterAllAreas();
          FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
          RouteConfig.RegisterRoutes(RouteTable.Routes);
