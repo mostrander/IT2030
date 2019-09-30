2d5ee6c1bf5dbef8e43b99538a41079548b12885
Natasha Ostrander <mostrander181@gmail.com>
2019-09-30 08:43:12 -0400

    Erased database entries; Added validation code to Student and Course classes

--- MOstrander_Lab04/EnrollmentApplication/EnrollmentApplication/Models/Course.cs
+++ MOstrander_Lab04/EnrollmentApplication/EnrollmentApplication/Models/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,20 @@ namespace EnrollmentApplication.Models
 {
    public class Course
    {
-
+      [Display(Name = "Course ID")]
       public virtual int CourseId { get; set; }
+
+      [Required]
+      [StringLength(150)]
+      [Display(Name = "Course Title")]
       public virtual string CourseTitle { get; set; }
+
+      [Display(Name = "Description")]
       public virtual string CourseDescription { get; set; }
+
+      [Required]
+      [Display(Name = "Number of credits")]
+      [Range(1,4)]
       public virtual int CourseCredits { get; set; }
 
    }
--- MOstrander_Lab04/EnrollmentApplication/EnrollmentApplication/Models/Student.cs
+++ MOstrander_Lab04/EnrollmentApplication/EnrollmentApplication/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,17 @@ namespace EnrollmentApplication.Models
 {
    public class Student
    {
-
+      [Display(Name = "Student ID")]
       public virtual int StudentId { get; set; }
+
+      [Required]
+      [StringLength(50)]
+      [Display(Name = "Last Name")]
       public virtual string LastName { get; set; }
+
+      [Required]
+      [StringLength(50)]
+      [Display(Name = "First Name")]
       public virtual string FirstName { get; set; }
 
    }
