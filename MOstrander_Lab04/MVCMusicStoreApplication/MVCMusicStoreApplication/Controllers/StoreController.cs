1b0963e362b788ef2f0d22059a537e212a75ccfd
Natasha Ostrander <mostrander181@gmail.com>
2019-10-28 09:37:44 -0400

    Changes for lab 10; created storecontroller & browse/details/index pages. Need to change index so it filters albums according to id passed to it, and details page is currently not working.

---
+++ MOstrander_Lab04/MVCMusicStoreApplication/MVCMusicStoreApplication/Controllers/StoreController.cs
@@ -0,0 +1,45 @@
+﻿using MVCMusicStoreApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMusicStoreApplication.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        [HttpGet]
+        public ActionResult Index(int id)
+        {
+            MVCMusicStoreDB db = new MVCMusicStoreDB();
+
+            return View(db.Albums.ToList());
+        }
+
+         [HttpGet]
+         public ActionResult Browse()
+         {
+            MVCMusicStoreDB db = new MVCMusicStoreDB();
+
+            return View(db.Genres.ToList());
+         }
+
+         [HttpGet]
+         public ActionResult Details(int id)
+         {
+
+            MVCMusicStoreDB db = new MVCMusicStoreDB();
+
+            Album album = db.Albums.Find(id);
+           
+            return View(album);
+
+
+         }
+
+
+    }
+}
