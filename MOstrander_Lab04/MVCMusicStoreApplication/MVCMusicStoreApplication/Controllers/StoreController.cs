f8317209d23ee07522c2f1f188b6b8da1cab49c7
Natasha Ostrander <mostrander181@gmail.com>
2019-10-28 11:15:24 -0400

    Fixed database search problems with index and browse pages for storecontroller; added details page layout and functionality; Just need to add CSS for page layout formatting and design. Lab 10 almost complete

--- MOstrander_Lab04/MVCMusicStoreApplication/MVCMusicStoreApplication/Controllers/StoreController.cs
+++ MOstrander_Lab04/MVCMusicStoreApplication/MVCMusicStoreApplication/Controllers/StoreController.cs
@@ -10,23 +10,35 @@ namespace MVCMusicStoreApplication.Controllers
 {
     public class StoreController : Controller
     {
-        // GET: Store
+        // Shows albums by requested Genre
         [HttpGet]
         public ActionResult Index(int id)
         {
             MVCMusicStoreDB db = new MVCMusicStoreDB();
 
-            return View(db.Albums.ToList());
+         return View (db.Albums
+            .Where(a => a.GenreId.Equals(id))
+            .OrderBy(a => a.Title)
+            .ToList());
+
         }
+
+
 
          [HttpGet]
          public ActionResult Browse()
          {
             MVCMusicStoreDB db = new MVCMusicStoreDB();
 
-            return View(db.Genres.ToList());
+            return View(db.Genres
+               .OrderBy(a => a.Name)
+               .ToList());
+
          }
 
+
+
+         //gets specific album
          [HttpGet]
          public ActionResult Details(int id)
          {
@@ -34,8 +46,9 @@ namespace MVCMusicStoreApplication.Controllers
             MVCMusicStoreDB db = new MVCMusicStoreDB();
 
             Album album = db.Albums.Find(id);
-           
+
             return View(album);
+
 
 
          }
