81aa5fc34573981390309d62f030545e039d008e
Natasha Ostrander <mostrander181@gmail.com>
2019-11-27 07:32:16 -0500

    Created the default project that I will build my final project upon. The goal is to replicate the general concept of an event website that sells tickets, but this project will not handle money so user will simply "register" for the events. Users can also create events and new event types.

---
+++ IT2030_MeganOstrander_FinalProject/IT2030_MeganOstrander_FinalProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IT2030_MeganOstrander_FinalProject.MvcApplication" Language="C#" %>
---
+++ IT2030_MeganOstrander_FinalProject/IT2030_MeganOstrander_FinalProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IT2030_MeganOstrander_FinalProject.Startup))]
+namespace IT2030_MeganOstrander_FinalProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
